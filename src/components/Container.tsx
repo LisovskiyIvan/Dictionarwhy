3e4c5ea3ac188c614b38327dab46c76582cf4079
Ivan Lisovskii <dnevanek@gmail.com>
2023-11-18 19:42:29 +0300

    first page design done

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,13 +2,12 @@ import { Textarea } from "./Textarea"
 
 export function Container () {
     return (
-        <div className="mt-28 mx-40 w-9/12 flex justify-between">
-            
+        <div className="mt-28 mx-40 w-9/12 flex justify-between items-center">
             <Textarea />
-            
-            
+            <div className="w-14 transition ease-in-out delay-50  hover:scale-110  duration-300 cursor-pointer">
+                <img src="/img/swap.png" alt="" />
+            </div>
             <Textarea />
-            
         </div>
     )
 }
--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -3,7 +3,14 @@ export function Textarea() {
     <div className="w-5/12 h-auto bg-white backdrop-blur-lg shadow-md rounded-md">
       <div className="flex p-3 justify-between">
       <h5 className="text-2xl">Название</h5>
-      <select className="text-md w-auto" name="Somename" id="">
+      <select className="text-md w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 " name="Somename" id="">
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
+        <option value="">someoptin</option>
         <option value="">someoptin</option>
       </select>
       </div>
